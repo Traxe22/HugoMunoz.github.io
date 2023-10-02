6cff4fa09fe607ba60b9b10cd3c1912e7af11d63
Hugo Muñoz <43103116+Traxe22@users.noreply.github.com>
2023-10-02 11:40:46 -0300

    arreglos

--- carrusel.js
+++ carrusel.js
@@ -1,3 +1,4 @@
+//Barca
 let array = ["carru/1.jpg","carru/2.jpg","carru/3.jpg","carru/4.jpg"];
     let im = document.getElementById("foto");
     let cont = 0;
@@ -18,4 +19,26 @@ let array = ["carru/1.jpg","carru/2.jpg","carru/3.jpg","carru/4.jpg"];
             cont++;
         }
         im.src = array[cont];
+    }
+//Paris
+    let array1 = ["carru1/1.jpg","carru1/2.jpg","carru1/3.jpg","carru1/4.jpg"];
+    let im1 = document.getElementById("foto1");
+    let cont1 = 0;
+
+    function anterior1(){
+        if(cont1===0){
+            cont1 = array1.length -1;
+        }else{
+            cont1--;
+        }
+        im1.src = array1[cont1];
+    }
+
+    function siguiente1(){
+        if(cont1===array1.length -1){
+            cont1 =0;
+        }else{
+            cont1++;
+        }
+        im1.src = array1[cont1];
     }
