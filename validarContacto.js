ab761f02ee961c5c18d86d533f3f7ff4c5546597
Hugo Muñoz <43103116+Traxe22@users.noreply.github.com>
2023-10-02 13:51:30 -0300

    menu

--- validarContacto.js
+++ validarContacto.js
@@ -1,11 +1,24 @@
-function validarContacto(){
+function validarContacto() {
     var nombre = document.getElementById("nombre").value;
     var telefono = document.getElementById("telefono").value;
     var gmail = document.getElementById("gmail").value;
-    var mensaje= document.getElementById("mensaje").value;
-    if(nombre==""|| telefono==""|| gmail==""|| mensaje==""){
-        alert("Completar todos los campos");
-    }else{
-        alert("Formulario enviado.");
+    var mensaje = document.getElementById("mensaje").value;
+    var datosUsuario = document.getElementById("datosUsuario");
+
+    //Validar el formato de gmail.
+    var valemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!valemail.test(gmail)){
+        errorMe
     }
+    if (nombre == "" || telefono == "" || gmail == "" || mensaje == "") {
+        alert("Completar todos los campos");
+    } else {
+        alert("Formulario enviado.");
+        var mensajeCompleto = "Datos Enviados:\n" +
+        "Nombre: " + nombre + "\n" +
+        "Telefono: " + telefono + "\n" +
+        "Gmail: " + gmail + "\n" +
+        "Comentario: " + mensaje + "\n";
+        document.getElementById("mensajeUsuario").innerHTML = mensajeCompleto;
+}
 }
