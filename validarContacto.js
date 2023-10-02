8a81f282c5c585359fbf3c8ab5df154a5b120935
Hugo Muñoz <43103116+Traxe22@users.noreply.github.com>
2023-10-02 17:43:20 -0300

    icons

--- validarContacto.js
+++ validarContacto.js
@@ -3,22 +3,27 @@ function validarContacto() {
     var telefono = document.getElementById("telefono").value;
     var gmail = document.getElementById("gmail").value;
     var mensaje = document.getElementById("mensaje").value;
-    var datosUsuario = document.getElementById("datosUsuario");
+    var datosUsuario = document.getElementById("mensajeUsuario");
 
-    //Validar el formato de gmail.
+    // Validar el formato de gmail.
     var valemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(!valemail.test(gmail)){
-        errorMe
-    }
-    if (nombre == "" || telefono == "" || gmail == "" || mensaje == "") {
+    if (!valemail.test(gmail)) {
+        alert("El correo electrónico no es válido");
+    } else if (nombre == "" || telefono == "" || gmail == "" || mensaje == "") {
         alert("Completar todos los campos");
     } else {
         alert("Formulario enviado.");
         var mensajeCompleto = "Datos Enviados:\n" +
-        "Nombre: " + nombre + "\n" +
-        "Telefono: " + telefono + "\n" +
-        "Gmail: " + gmail + "\n" +
-        "Comentario: " + mensaje + "\n";
-        document.getElementById("mensajeUsuario").innerHTML = mensajeCompleto;
-}
+            "Nombre: " + nombre + "\n" +
+            "Telefono: " + telefono + "\n" +
+            "Gmail: " + gmail + "\n" +
+            "Comentario: " + mensaje + "\n";
+        datosUsuario.innerHTML = mensajeCompleto;
+
+        // Limpiar los campos
+        document.getElementById("nombre").value = "";
+        document.getElementById("telefono").value = "";
+        document.getElementById("gmail").value = "";
+        document.getElementById("mensaje").value = "";
+    }
 }
